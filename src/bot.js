4a36e68268130270f24910eeb18bfb4651909df2
spookyhub <george@ghabro.nl>
2023-03-22 23:06:15 +0100

    intents.js

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,7 @@
 import { Client, GatewayIntentBits,SlashCommandBuilder, Events } from 'discord.js';
+import myIntents from '../intents.js';
 
-const client = new Client({
-   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ] 
-  });
+const client = new Client({ intents: myIntents });
 
 client.once(Events.ClientReady, async c => {
 	console.log(`${c.user.tag} is up running`);
---
+++ src/intents.js
@@ -0,0 +1,7 @@
+import {GatewayIntentBits} from 'discord.js'
+
+export default [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]
