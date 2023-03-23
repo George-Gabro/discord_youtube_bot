3a7ef016ff8ae4ca02df32007027423d3fd16966
spookyhub <george@ghabro.nl>
2023-03-23 13:12:10 +0100

    add protocol list

---
+++ src/protocol/protocol.js
@@ -0,0 +1,11 @@
+const STOP_MUSIC_COMMAND = "music stop";
+const PLAY_MUSIC_COMMAND = "music play";
+const PAUSE_MUSIC_COMMAND = "music pause"
+const RESUME_MUSIC_COMMAND = "music resume"
+
+export {
+    STOP_MUSIC_COMMAND,
+    PLAY_MUSIC_COMMAND,
+    PAUSE_MUSIC_COMMAND,
+    RESUME_MUSIC_COMMAND
+}
